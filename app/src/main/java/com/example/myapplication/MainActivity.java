8d79a2a26da8a4f7d45b2eab1b905d68b3b8c847
Nazir Knuckles <Batking74@gmail.com>
2022-11-18 16:32:08 -0500

    Added some more conditions and functionality using Java

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,14 +2,21 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+
 import android.os.Bundle;
 
 import android.util.Log;
 
 import android.view.View;
+
 import android.widget.Button;
+
 import android.widget.Switch;
+
 import android.widget.TextView;
+
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,22 +32,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void validateMainActivity(View view) {
+    public void signIn(View view) {
         Switch mySwitch = findViewById(R.id.SwitchView);
-        String usersUsername = ((TextView)findViewById(R.id.UsernameInputView)).getText().toString();
-        String usersPassword = ((TextView)findViewById(R.id.PasswordInputView)).getText().toString();
-        String [] userData = {usersUsername, usersPassword};
-        String [] message = {" Your Credentials Have Been Saved!", " Thank You, Your Credentials Will Not Be Remembered!", "Done!", "User Requested not to have their Credentials saved"};
-        ((TextView)findViewById(R.id.TittleView)).setText("Welcome " + usersUsername + "!");
-        findViewById(R.id.SignInButton).setEnabled(false);
-        ((Button)findViewById(R.id.SignInButton)).setText(message[2]);
-        validateSwitch(mySwitch, userData[0], userData[1], message[0], message[1], message[3]);
+        String username = ((TextView)findViewById(R.id.UsernameInputView)).getText().toString();
+        String password = ((TextView)findViewById(R.id.PasswordInputView)).getText().toString();
+        validateUserInput(username, password, mySwitch);
+        }
+
+    @SuppressLint("SetTextI18n")
+    public void validateUserInput(String username, String password, Switch mySwitch) {
+        if(username.isEmpty() && password.isEmpty()) {
+            Toast.makeText(this, "Username and password is required!", Toast.LENGTH_LONG).show();
+        }
+        else if(username.isEmpty()) {
+            Toast.makeText(this, "Username is required!", Toast.LENGTH_LONG).show();
+        }
+        else if(password.isEmpty()) {
+            Toast.makeText(this, "Password is required!", Toast.LENGTH_LONG).show();
+        }
+        else {
+            ((TextView)findViewById(R.id.TittleView)).setText("Welcome " + username + "!");
+            findViewById(R.id.SignInButton).setEnabled(false);
+            String [] message = {" Your Credentials Have Been Saved!", " Thank You, Your Credentials Will Not Be Remembered!", "Done!", "User Requested not to have their Credentials saved"};
+            ((Button)findViewById(R.id.SignInButton)).setText(message[2]);
+            validateSwitch(mySwitch, username, password, message[0], message[1], message[3]);
+            Intent myIntent = new Intent(this, WelcomeActivity.class);
+            startActivity(myIntent);
+        }
     }
 
-    public void validateSwitch(Switch rememberMeSwitch, String username, String password, String saveCredentialMessage, String ignoreCredentialMessage, String ignoreCredentialMessageTwo) {
+    public void validateSwitch(@SuppressLint("UseSwitchCompatOrMaterialCode") Switch rememberMeSwitch, String username, String password, String saveCredentialMessage, String ignoreCredentialMessage, String ignoreCredentialMessageTwo) {
         if(rememberMeSwitch.isChecked()) {
-            Log.i("User Info", "Username is: " + username);
-            Log.i("User Info", "Password is: " + password);
+            Log.e("User Info", "Username is: " + username);
+            Log.e("User Info", "Password is: " + password);
             Toast.makeText(this, "Hello " + username + saveCredentialMessage, Toast.LENGTH_LONG).show();
         }
         else {
